e5bef0ee2ef80774b88b3c6c01f7993c7b864833
xiaoqiang <3160101819@zju.edu.cn>
2020-04-17 23:33:15 -0400

    batch rectify/refine

--- code/utils/config.py
+++ code/utils/config.py
@@ -79,10 +79,10 @@ arg_config = {
 }
 ################################################################################
 
-# summary_key = 'exp-full-channel-so-0' #in output-backup directory: 2 times middle channel njud-nlpr 180 eras
-# summary_key = 'exp-reduce-channel-mt-0' #: 1 time middle channel njud-nlpr 500 eras
-# summary_key = 'exp-reduce-channel-so-0' #: 1 time middle channel njud-nlpr 180 eras
-summary_key = 'exp-reduce-channel-so-1' #: 1 time middle channel njud-nlpr 500 eras
+# summary_key = 'exp-full-channel-so-0' #in output-backup directory: 2 times middle channel njud-nlpr 180 eras / batch 4
+# summary_key = 'exp-reduce-channel-mt-0' #: 1 time middle channel njud-nlpr 500 eras / batch 4+4 ? how about 4+2 / 6+2
+# summary_key = 'exp-reduce-channel-so-0' #: 1 time middle channel njud-nlpr 180 eras / batch 4
+summary_key = 'exp-reduce-channel-so-1' #: 1 time middle channel njud-nlpr 500 eras / batch 8
 # summary key solves other varients(supervised only or MT guiding unlabel data)
 ckpt_path = os.path.join(os.path.dirname(proj_root), 'output', summary_key)
 
