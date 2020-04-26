36ab3e70af4e48044570dbbfbb0b5bfbacf16331
xiaoqiang <3160101819@zju.edu.cn>
2020-04-26 10:26:15 -0400

    delayed rampup / batch 4

--- code/utils/config.py
+++ code/utils/config.py
@@ -68,13 +68,13 @@ arg_config = {
     'lr_type': 'poly',
     'lr_decay': 0.9,  # poly
     'use_bigt': True,  # 有时似乎打开好，有时似乎关闭好？
-    'batch_size': 8,  # 要是继续训练, 最好使用相同的batchsize
+    'batch_size': 4,  # 要是继续训练, 最好使用相同的batchsize
     'num_workers': 8,  # 不要太大, 不然运行多个程序同时训练的时候, 会造成数据读入速度受影响
     'input_size': 256,
     'gpus': [0, 1],
 
     'is_mt': None, # set in main.py or main_mt.py
-    'labeled_batch_size': 4,
+    'labeled_batch_size': 2,
     'ema_decay': 0.99,
     'consistency': 1.0,
     'consistency_rampup': 300
@@ -82,7 +82,7 @@ arg_config = {
 ################################################################################
 
 # summary_key = 'exp-full-channel-so-0' #in output-backup directory: 2 times middle channel njud-nlpr 180 eras / batch 4
-# summary_key = 'exp-reduce-channel-mt-0' #: 1 time middle channel njud-nlpr 500 eras / 150 rampup / fine tune
+# summary_key = 'exp-reduce-channel-mt-2-0' #: 1 time middle channel njud-nlpr 500 eras / 150 rampup / fine tune
 summary_key = 'exp-reduce-channel-mt-2' #: 1 time middle channel njud-nlpr 350 eras / 150 rampup / fusion based consistency
 # summary_key = 'exp-reduce-channel-so-0' #: 1 time middle channel njud-nlpr 180 eras / batch 4
 # summary_key = 'exp-reduce-channel-so-1' #: 1 time middle channel njud-nlpr 500 eras / batch 8
